15a710d0ee6d065ad71f3ed728c9778ca7ce348b
lolotrgeek <jon@simplyenable.com>
2018-04-28 19:31:00 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+*Incomplete*
+
+Just messing around with another idea and embodied recommendation engines
---
+++ predict.py
@@ -0,0 +1,14 @@
+
+
+1 = action
+2 = location
+3 = time
+4 = demographic
+
+
+
+# looks like you're in this location doing action at such time, I predict...
+
+# you, or other people like, usually do this action at such time and location, I predict...
+
+# predictions are numeric, rules are used to assign non-numeric actions to the predictions
